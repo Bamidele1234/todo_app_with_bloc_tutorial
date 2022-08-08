21e379a585760956d67caeaa3c04d5c133ef4be1
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-08-08 10:50:02 +0100

    Finished the tutorial project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_with_bloc_tutorial/ui/home_page.dart';
 
 main() {
   runApp(
@@ -8,13 +9,18 @@ main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Reactive Flutter',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        canvasColor: Colors.transparent,
+      ),
+      //Our only screen/page we have
+      home: HomePage(
+        title: 'My Todo List',
       ),
     );
   }
